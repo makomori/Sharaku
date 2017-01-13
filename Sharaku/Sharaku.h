27969dff8ef3d5b792902b3a6e4910ea7b4a42ca
Ruben Roques <rubenroques.nhk@gmail.com>
2017-01-13 00:30:01 +0000

    Removed example. Added Framework project.

---
+++ Sharaku/Sharaku.h
@@ -0,0 +1,19 @@
+//
+//  Sharaku.h
+//  Sharaku
+//
+//  Created by Ruben Roques on 13/01/2017.
+//  Copyright © 2017 Ruben Roques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sharaku.
+FOUNDATION_EXPORT double SharakuVersionNumber;
+
+//! Project version string for Sharaku.
+FOUNDATION_EXPORT const unsigned char SharakuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sharaku/PublicHeader.h>
+
+
